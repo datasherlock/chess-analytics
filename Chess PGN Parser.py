9860f88ac9e9be1e8258db128467099ed730cba1
Jerome Rajan <jerome.rajan1987@gmail.com>
2021-01-05 21:28:19 +0530

    Add files via upload

--- Chess PGN Parser.py
+++ Chess PGN Parser.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[115]:
-
+#Import libraries
 
 import re
 import pandas as pd
@@ -13,27 +12,19 @@ import urllib.request
 from pathlib import Path
 
 
-# In[69]:
-
-
-#Globals
-
-#filepath = "./Documents/apichess.pgn"
-pgnMeta = ["Event","Site","Date","Round","White","Black","Result","CurrentPosition","Timezone",            "ECO","ECOURL","UTDate","UTCTime","WhiteELO","BlackELO","Timecontrol","Termination","StartTime","EndDate","EndTime","Link","Moves"]
-tgtFilePath="./Documents/mygames.csv"
-moveStartLine = 22
-PGNDirectory="./Documents/PGN"
-user='datasherlock'
-
-
-# In[112]:
+#Declare Globals
+pgnMeta = ["Event","Site","Date","Round","White","Black","Result", \
+            "CurrentPosition","Timezone", "ECO","ECOURL","UTDate","UTCTime","WhiteELO", \
+            "BlackELO","Timecontrol","Termination","StartTime","EndDate","EndTime","Link","Moves"]
+tgtFilePath="./Documents/mygames.csv" #This is the path where the final CSV gets created
+moveStartLine = 22 #Moves in chess.com PGNs typically start from the 22nd line for each game
+PGNDirectory="./Documents/PGN" #This is the location where the API downloads the PGNs from the archives
+user='datasherlock' #The user for whom the script is intended to run
 
 
 def getPGN(user):
+    """This function accesses the chess.com public API and downloads all the PGNs to a folder"""
     pgn_archive_links = requests.get("https://api.chess.com/pub/player/"+user+"/games/archives", verify=False)
-    
-   
-    
     if not os.path.exists(PGNDirectory):
         os.makedirs(PGNDirectory)
 
@@ -43,19 +34,13 @@ def getPGN(user):
         if not my_file.is_file():
             urllib.request.urlretrieve(url+'/pgn',filepath)
 
-
-# In[71]:
-
-
 def importPGNData(filepath):
+    """This function returns the data read as a string"""
     with open(filepath) as f:
         return f.readlines()
 
-
-# In[72]:
-
-
 def getEdgePoints(data):
+    """This function returns the start and end indices for each game in the PGN"""
     ends=[]
     starts=[]
     for n,l in enumerate(data):
@@ -65,28 +50,23 @@ def getEdgePoints(data):
             starts.append(n)
         elif (n==len(data)-1):
             ends.append(n)
-            
+
     return (starts,ends)
 
 
-# In[73]:
-
-
 def grpGames(data, starts, ends):
+    """This function groups games into individual lists based on the start and end index"""
     blocks=[]
     for i in range(len(ends)):
         try:
             element = data[starts[i]: ends[i]+1]
         except:
             print(i)
-        if element not in blocks: blocks.append(element)        
+        if element not in blocks: blocks.append(element)
     return blocks
 
-
-# In[107]:
-
-
 def mergeMoves(game):
+    """This function cleans out the moves and other attributes, removes newlines and formats the list to be converted into a dictionary"""
     firstmove=lastmove=-1
     for n,eachrow in enumerate(game):
                 game[n]=game[n].replace('\n','')
@@ -95,14 +75,7 @@ def mergeMoves(game):
                 except:
                     if n <= moveStartLine-4: game[n] = stripwhitespace(game[n]).split('~')[1].strip(']["')
                     pass
-            
-   # moves = ' '.join(game[firstmove:lastmove+2])
-   # del game[firstmove:lastmove+2]
-   # game.append(moves)
     return list(filter(None,game))
-
-
-# In[101]:
 
 
 def stripwhitespace(text):
@@ -112,47 +85,40 @@ def stripwhitespace(text):
             lst[i] = re.sub("\s+", "~", item)
     return '"'.join(lst)
 
-
-# In[102]:
-
-
 def createGameDictLetsPlay(game_dict):
-        for n, move in enumerate(game_dict["Moves"].split(" ")):
-            
-            if n%3==0:
-                if move == '1-0' or move=='0-1' or move=='1/2-1/2':
-                    None
-                else: movenum = n 
-            elif n==movenum+2:
-                if move == '1-0' or move=='0-1' or move=='1/2-1/2':
-                    None
-                else: game_dict["whitemoves"].append(move)
-            else:
-                if move == '1-0' or move=='0-1' or move=='1/2-1/2':
-                    None
-                else: game_dict["blackmoves"].append(move)
-        
-        if len(game_dict["blackmoves"])>len(game_dict["whitemoves"]): game_dict["whitemoves"].append("over")
-        if len(game_dict["blackmoves"])<len(game_dict["whitemoves"]): game_dict["blackmoves"].append("over")
-        del game_dict["Moves"]      
-        return game_dict
+    """This is a helper function to address games under Lets Play events on chess.com. These events have a slightly different way of representation than the Live Chess events"""
+    for n, move in enumerate(game_dict["Moves"].split(" ")):
 
-        
+        if n%3==0: #every 3rd element is the move number
+            if move == '1-0' or move=='0-1' or move=='1/2-1/2':
+                None
+            else: movenum = n
+        elif n==movenum+2:
+            if move == '1-0' or move=='0-1' or move=='1/2-1/2':
+                None
+            else: game_dict["whitemoves"].append(move)
+        else:
+            if move == '1-0' or move=='0-1' or move=='1/2-1/2':
+                None
+            else: game_dict["blackmoves"].append(move)
 
-
-# In[109]:
+    if len(game_dict["blackmoves"])>len(game_dict["whitemoves"]): game_dict["whitemoves"].append("over")
+    if len(game_dict["blackmoves"])<len(game_dict["whitemoves"]): game_dict["blackmoves"].append("over")
+    del game_dict["Moves"]
+    return game_dict
 
 
 def createGameDictLiveChess(game_dict):
+    """This is a helper function to address games under Live Chess events on chess.com."""
     try:
         for n, move in enumerate(game_dict["Moves"].split(" ")):
 
-            if '{' in move or '}' in move: 
+            if '{' in move or '}' in move:
                 None
             elif '.' in move:
                 movenum = int(move.split(".")[0])
-                if "..." in move: 
-                    color = 'black' 
+                if "..." in move:
+                    color = 'black'
                 else: color="white"
             else:
                 if color=="white":
@@ -168,10 +134,6 @@ def createGameDictLiveChess(game_dict):
     except: pass
 
     return game_dict
-        
-
-
-# In[104]:
 
 
 def createGameDict(games):
@@ -181,23 +143,19 @@ def createGameDict(games):
         movenum = 0
         game_dict["whitemoves"] = []
         game_dict["blackmoves"] = []
-        game_dict["Result"]=""
         color="white"
         if game_dict["Event"]=="Let's Play!": allgames.append(createGameDictLetsPlay(game_dict))
         else: allgames.append(createGameDictLiveChess(game_dict))
-    
+
     return allgames
-        
 
-
-# In[122]:
 
 
 def main():
     getPGN(user)
     tgtFilePathObj = Path(tgtFilePath)
     tgtFilePathObj.unlink(missing_ok=True)
-    
+
     with os.scandir(PGNDirectory) as pgndir:
         for file in pgndir:
             print('*', end =" ")
@@ -215,15 +173,5 @@ def main():
                     df.to_csv(f, mode='a', header=f.tell()==0)
     print("Export Complete!")
 
-
-# In[124]:
-
-
+#Run Program
 main()
-
-
-# In[ ]:
-
-
-
-
